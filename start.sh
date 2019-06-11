f4c17281a2e2e6ccf5b1793d444e840ce69e4f95
CircleCI <circleci@circleci.com>
2019-06-11 23:40:34 +0800

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
+#! For Ubuntu 
+
+curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
+chmod u+x prereqs-ubuntu.sh
+sudo ./prereqs-ubuntu.sh
+
+#!   NODEJS  8 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install nodejs
+
 sudo npm install -g composer-cli --unsafe-perm=true --allow-root
 sudo npm install -g yo --unsafe-perm=true --allow-root
 sudo npm install -g generator-hyperledger-composer --unsafe-perm=true --allow-root
@@ -12,6 +22,6 @@ curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/pack
 
 tar -xvf fabric-dev-servers.tar.gz
 
-./downloadFabric.sh
-./startFabric.sh
-./createPeerAdminCard.sh
+sudo ./downloadFabric.sh
+sudo ./startFabric.sh
+sudo ./createPeerAdminCard.sh
